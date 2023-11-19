7d993d865ec1ff03ab3b6393766a1975b67cc8f9
RBCodeJourney <refatullahbiswas@gmail.com>
2023-11-19 11:08:20 +0530

    first commit with types.js file added

---
+++ types.js
@@ -0,0 +1,15 @@
+// strongly typed Language
+
+// int a = 5;
+// string b = 'alilm halim khalim'
+// bool c = True;
+// object student = {name: 'noya dhman', id: 55}
+// int[] numbers = [12, 45, 78]
+// string[] friends = ['abul', 'babul']
+
+// dynamic type Language
+const a = 5;
+const b = 'Samsu kopain na ekhon ar';
+const ages = [45, 65, 48];
+const student = { id: 23, class: 7 }
+console.log(typeof a, typeof b, typeof ages, typeof student);
